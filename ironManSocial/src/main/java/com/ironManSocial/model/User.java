42a5cef5326e3af899d99478eda8583da3178bfc
preetitrehan <preetitrehan@192.168.0.101>
2020-02-25 20:45:25 +0530

    #3: Correcting the formatting of comments (removing extra white space). Adding default constructor( will be required for dependency injection through property)
    Adding public modifier for parameterized constructor

--- ironManSocial/src/main/java/com/ironManSocial/model/User.java
+++ ironManSocial/src/main/java/com/ironManSocial/model/User.java
@@ -1,52 +1,56 @@
-/*			Basic User Class								*/
 
 package com.ironManSocial.model;
 
+/*Basic User Class*/
+
 public class User {
 
+	/* Instance Variable declaration */
 
-/*			Instance Variable declaration					*/
-	
 	private String firstName;
 	private String lastName;
 	private String userName;
-	
+
 	private String userPassword;
-	
+
 	private int userAge;
-	
+
 	private String userEmail;
-	
-	private String Gender;
 
+	private String gender;
 
-/*			User Parameterized Constructor Creation			*/	
-	
-	User(String firstName, String lastName, String userName, String userPassword, int userAge, String userEmail, String Gender){
+	// Default constructor
+	public User() {
+
+	}
+
+	/* User parameterized constructor Creation */
+
+	public User(String firstName, String lastName, String userName, String userPassword, int userAge, String userEmail,
+			String gender) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.userName = userName;
-		this.userPassword =userPassword;
+		this.userPassword = userPassword;
 		this.userAge = userAge;
 		this.userEmail = userEmail;
-		this.Gender = Gender;
+		this.gender = gender;
 	}
-	
 
-/*			Setters & Getters for instance variable		*/
-	
+	/* Setters & Getters for instance variables */
+
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
@@ -54,11 +58,11 @@ public class User {
 	public String getUserName() {
 		return userName;
 	}
-	
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
+
 	public String getUserPassword() {
 		return userPassword;
 	}
@@ -66,28 +70,28 @@ public class User {
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
-	
+
 	public int getUserAge() {
 		return userAge;
 	}
-	
+
 	public void setUserAge(int userAge) {
 		this.userAge = userAge;
 	}
-	
+
 	public String getUserEmail() {
 		return userEmail;
 	}
-	
+
 	public void setUserEmail(String userEmail) {
 		this.userEmail = userEmail;
 	}
-	
+
 	public String getGender() {
-		return Gender;
+		return gender;
 	}
-	
-	public void setGender(String Gender) {
-		this.Gender = Gender;
+
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 }
